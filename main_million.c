e345eff14481a98a08a6a91a7ebeca88b0b5abc3
Debajyoti Dash <debajyoti.dash3011@gmail.com>
2017-05-11 00:48:48 -0700

    Create main_million.c
    This file contains the main function that calls FloatAdd.c file with million combinations of int or float numbers. FloatAdd.c file returns the result of addition of 2 numbers to main function.

---
+++ main_million.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int FloatAdd(int, int);
+
+int main(void)
+{
+
+  // float f1 = 2.0;
+  // float f2 = 1.0;
+  srand(time(NULL));
+  int i;
+  for (i=0; i<1000000;i++){
+    
+    int x = rand();
+    int y = rand();
+
+  int iresult = FloatAdd(x, y);
+  // int iresult = FloatAdd(*(int *)&f1, *(int *)&f2);
+
+  // printf("%08x\n", iresult);
+  float fresult = (*(float *) &x) + (*(float *) &y);;
+  // float fresult = f1+f2;
+
+  printf("%08x + %08x --> %08x (should be %08x)\n", x, y, iresult,
+   *(int *)&fresult);
+  if (iresult != *(int *)&fresult){
+    fprintf(stderr, "***ERROR***\n");
+    fprintf(stderr, "***Calculation Performed***: %i \n", i); 
+    return 0;
+  }
+
+  // printf("%08x + %08x --> %08x (should be %08x)\n", *(int *)&f1, *(int *)&f2, iresult,
+  //   *(int *)&fresult);
+ }
+ fprintf(stderr, "***Calculation Performed***: %i \n", i); 
+  return 0;
+}
